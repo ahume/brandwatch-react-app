210e6b2f77973220563fd71e42144d237ca14e2d
thatguynamedandy <andyp@brandwatch.com>
2017-08-25 14:08:05 +0100

    chore(Redux): Wire up redux store with router and test component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import reducers from './store';
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const store = createStore(
+  reducers,
+  composeWithDevTools(
+    applyMiddleware(middleware)
+  ),
+);
 
 function render(Component) {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Provider store={ store }>
+        <ConnectedRouter history={ history }>
+          <div>
+            <Route component={ Component } exact path="/" />
+          </div>
+        </ConnectedRouter>
+      </Provider>
     </AppContainer>,
     document.getElementById('root')
   );
@@ -15,5 +37,5 @@ function render(Component) {
 render(App);
 
 if (module.hot) {
-  module.hot.accept('./App', () => render(App));
+  module.hot.accept('./containers/App', () => render(App));
 }

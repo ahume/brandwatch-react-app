990a4306d0168586a5b4238810313681ee543d81
Benji Hall <benji@brandwatch.com>
2017-09-04 17:20:01 +0100

    NOTICKET chore(babel): Use babel-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,14 @@ module.exports = {
   plugins: [
     require('postcss-import')(),
     require('postcss-cssnext')({
-      browsers: require('bw-axiom/browsers'),
+      // copy pasted from .babelrc
+      browsers: [
+        'last 2 Firefox versions',
+        'last 2 Chrome versions',
+        'last 2 Edge versions',
+        'last 2 Safari versions',
+        'IE 11',
+      ],
       features: {
         customProperties: {
           warnings: false,

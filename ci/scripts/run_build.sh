23f9d6034e52dda2623167eb3d1e494e467454fa
thatguynamedandy <andyp@brandwatch.com>
2017-09-11 11:08:34 +0100

    GCP stage deployment

--- ci/scripts/run_build.sh
+++ ci/scripts/run_build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-
 set -e -u -x
 
-export CURRENT_TIMESTAMP=$(date +"%Y%m%d%H%S")
 mv dependency-cache/node_modules brandwatch-react-app
 cd brandwatch-react-app && yarn build
-tar -cvzf ./brandwatch-react-app-$CURRENT_TIMESTAMP.tar.gz  --directory=./public .
-# cp ./brandwatch-react-app-$CURRENT_TIMESTAMP.tar.gz ../
-# cp ./brandwatch-react-app-$CURRENT_TIMESTAMP.tar.gz ./public
- cp ./brandwatch-react-app-$CURRENT_TIMESTAMP.tar.gz ../public
+tar -cvzf ./brandwatch-react-app-1.tar.gz  --directory=./public .
+ cp ./brandwatch-react-app-1.tar.gz ../public

695b2b510fed9966159116a2206065b7d104e70e
thatguynamedandy <andyp@brandwatch.com>
2017-09-12 16:03:54 +0100

    Added placeholder prod token

---
+++ DEPLOYMENT.md
@@ -0,0 +1,3 @@
+
+
+Any commits that contain the string [ci skip] will be ignored. This allows you to commit to your repository without triggering a new version.
--- ci/scripts/run_build.sh
+++ ci/scripts/run_build.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 set -e -u -x
 
+env
+
 mv dependency-cache/node_modules brandwatch-react-app
 cd brandwatch-react-app && yarn build
-tar -cvzf ./brandwatch-react-app-1.tar.gz  --directory=./public .
+
+echo .git/refs/heads
+ls -lrt
+
+export version = $(<.git/refs/heads/gcp-integration)
+
+echo 'version'
+echo $version
+
+tar -cvzf ./brandwatch-react-app.$version.tar.gz  --directory=./public .
  cp ./brandwatch-react-app-1.tar.gz ../public

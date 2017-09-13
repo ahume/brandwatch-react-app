c51bec8ee7634f9b1ce3690e0bb48a39af3affef
thatguynamedandy <andyp@brandwatch.com>
2017-09-13 13:19:47 +0100

    Move public to build

--- ci/scripts/run_build.sh
+++ ci/scripts/run_build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e -u -x
 
-env
+mv dependency-cache/node_modules brandwatch-react-app
+cd brandwatch-react-app && yarn build
 
 VERSION=$(<.git/refs/heads/gcp-integration)
 RESOURCE=brandwatch-react-app.$VERSION.tar.gz
@@ -9,8 +10,5 @@ RESOURCE=brandwatch-react-app.$VERSION.tar.gz
 echo "$VERSION"
 echo "$RESOURCE"
 
-mv dependency-cache/node_modules brandwatch-react-app
-cd brandwatch-react-app && yarn build
-
 tar -cvzf $RESOURCE  --directory=./public .
-cp ./brandwatch-react-app.$VERSION.tar.gz ../public
+cp ./brandwatch-react-app.$VERSION.tar.gz ../build
